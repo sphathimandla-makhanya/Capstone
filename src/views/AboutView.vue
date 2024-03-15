c96d04de203b4f1199390649c33ef11a67e38a80
sphathimandla-makhanya <makhanyasm11@gmail.com>
2024-03-15 13:12:57 +0200

    working on users component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="contain">
+ 
+  
+</div>
   </div>
 </template>
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="home w-50">
-    <!-- <img src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" alt=""> -->
-    <div class="shrinkwrap">
-<div class="">
-  <!-- <img class="img-top" src="https://source.unsplash.com/7r1HxvVC7AY/450x800" > -->
-  <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
-  <!-- <img class="img-bottom" src="https://source.unsplash.com/bULD0lNVXOA/450x800" > -->
-</div>
+  <div>
+    <div class="home w-100">
+      <!-- <img src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" alt=""> -->
+    </div>
+    <div class="contain">
+      <div class="marquee">
+        <div class="marquee-items"> 
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+
+    </div>
+    <!-- <div class="marquee-items"> 
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+      <img class="img-mid" src="https://i.ibb.co/DbH8RrC/f03dfa13d6578bbb17df55d29745e9b2.png" >
+    </div> -->
   </div>
+    </div>
   </div>
+
+
 </template>
 
 <script>
@@ -28,72 +44,40 @@ export default {
   background-image: url(https://i.ibb.co/t86Mn89/iceland-2111811-1920sd.jpg);
   background-repeat: no-repeat;
   background-size: cover;
-  height: 100vh;
+  height: 50vh;
+}
+.contain {
+  background-color: black;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  overflow: hidden;
+  height: 40vh;
 }
 
-* {
-  box-sizing:border-box;
+.marquee-items {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 
-body {
-  margin:0;
-  min-height:100vh;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  background:#0f0f0f;
-  perspective: 1000px;
-  overflow:hidden;
+.marquee {
+  display: flex;
+  width: 200%;
+  animation: marqueeSlide 20s linear infinite;
 }
 
-.container {
-  position:relative;
-  width:450px;
-  height:800px;
-  transform: rotateY(0deg) rotateX(0deg) scale(0.5); 
-  transform-style: preserve-3d;
-  animation:30s flipme linear infinite;
-  transition:transform 300ms ease;
-  transform-origin:50% 50%;
+@keyframes marqueeSlide {
+    from { transform: translateX(0); }
+    to { transform: translateX(-50%); }
 }
 
-.shrinkwrap {
-  transform:scale(0.5);
+p {
+  color: white;
+  font-size: 30px;
+}
+img{
+  height: 50%;
 }
 
-.container:hover {
-  transform: rotateY(30deg) rotateX(5deg);
-  cursor:pointer;
-}
-
-@keyframes flipme {
-  0% {
-    transform: rotateY(-30deg) rotateX(0deg);
-  }
-  50% {
-    transform: rotateY(0deg) rotateX(10deg);
-  }
-  100% {
-    transform: rotateY(-30deg) rotateX(0deg);
-  }
-   
-}
-
-.container>img {
-  position:absolute;
-}
-
-.container>img:nth-of-type(1) {
-  clip-path: polygon(0 0, 0 200px, 200px 0);
-  transform: translateZ(50px);
-}
-
-.container>img:nth-of-type(2) {
-  clip-path: polygon(250px 0%, 100% 0, 100% 550px, 150px 100%, 0 100%, 0% 250px);
-}
-
-.container>img:nth-of-type(3) {
-  clip-path: polygon(100% 600px, 200px 100%, 100% 100%);
-  transform: translateZ(-50px);
-}
 </style> 
